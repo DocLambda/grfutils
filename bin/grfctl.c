b893cb1177eab552f97c2879b66ef486dca0927a
Mirko Lindner <demon@pro-linux.de>
2015-12-28 14:44:38 +0100

    Set correct return type for getopt_long()

--- bin/grfctl.c
+++ bin/grfctl.c
@@ -96,7 +96,7 @@ int main(int argc, char **argv)
 	int            loglevel = GRF_DEFAULT_LOGLEVEL;
 	int            index;
 	int            ret;
-	char           c;
+	int            c;
 
 	static struct option options[] =
 	{
